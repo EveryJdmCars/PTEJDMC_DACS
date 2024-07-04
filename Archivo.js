a24ffe0b26e93b2b8b280d3a1140690dec57c70c
EveryJdmCars_Oficial <163068173+EveryJdmCars@users.noreply.github.com>
2024-07-04 11:12:00 -0500

    Add files via upload

---
+++ Archivo.js
@@ -0,0 +1,6 @@
+//-------------------------------------------------------------------------Funciones enel sitio web
+/* Mensaje de alerta para mostrar cuando aun no he terminado mi sitio web*/
+function Construyendo()
+{
+    alert("Sitio web en construccion"); 
+}
